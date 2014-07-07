2688ec7be1166e1001cf0a8acfa9550ebb262b80
tkorays <tkorays@hotmail.com>
2014-07-07 20:17:06 +0800

    resources

---
+++ widget/uitest/resources.h
@@ -0,0 +1,15 @@
+/**
+ * @file resources.h
+ * @autor tkorays
+ * @date 2014/7/7
+ * 
+ * @brief 定义资源文件
+ */
+#ifndef _TK_RESOURCES_H_
+#define _TK_RESOURCES_H_
+
+#define PATH_INIT_JPG "init.jpg"
+#define PATH_ICON_PNG "icon.png"
+
+
+#endif // _TK_RESOURCES_H_
